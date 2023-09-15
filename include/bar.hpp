03213532324cc2a9ba683b69705bcd45746660ed
yaamai <ya_amai@yahoo.co.jp>
2023-09-16 02:35:16 +0900

    impl bar-wide mouse scroll actions

--- include/bar.hpp
+++ include/bar.hpp
@@ -69,6 +69,7 @@ class Bar {
   void setVisible(bool value);
   void toggle();
   void handleSignal(int);
+  bool handleScroll(GdkEventScroll* e);
 
   struct waybar_output *output;
   Json::Value config;
@@ -110,6 +111,9 @@ class Bar {
   Gtk::Box center_;
   Gtk::Box right_;
   Gtk::Box box_;
+  double distance_scrolled_x_;
+  double distance_scrolled_y_;
+  Gtk::EventBox event_box_;
   std::vector<std::shared_ptr<waybar::AModule>> modules_left_;
   std::vector<std::shared_ptr<waybar::AModule>> modules_center_;
   std::vector<std::shared_ptr<waybar::AModule>> modules_right_;
